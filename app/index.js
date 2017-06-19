a5e4c755c610052ecae89ceafc9abdbd8dee37b5
toanvn <vantoan23@gmail.com>
2017-06-19 15:58:00 +0700

    add popular menu

--- app/index.js
+++ app/index.js
@@ -5,34 +5,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types')
 require('./index.css');
-
-class Users extends React.Component {
-    render() {
-        return (
-            <ul>
-                {this.props.list.map(function (friend) {
-                    return <li key={friend}>{friend}</li>
-                })}
-            </ul>
-        )
-    }
-}
-
-Users.propTypes = {
-    list: PropTypes.array.isRequired
-}
-
-class App extends React.Component{
-
-    render(){
-        return (
-            <div>
-                <Users list="Tyler, Jake, Mikenzi" />
-                <div>Hello {this.props.name}</div>
-            </div>
-        )
-    }
-}
+var App = require('./components/App');
 
 ReactDOM.render(
     <App name="Olal mamam" />, document.getElementById('app')
